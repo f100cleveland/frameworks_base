61f7717eb627054a81f0858cb1223dbd892f0013
Corey Edwards <ensabahnur16@gmail.com>
2016-03-23 14:17:59 -0500

    Nein!!!!
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/PhoneStatusBar.java
@@ -433,12 +433,9 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
     int mInitialTouchX;
     int mInitialTouchY;
 
-    private int mBatterySaverWarningColor;
-
-    // last theme that was applied in order to detect theme change (as opposed
-    // to some other configuration change).
-    ThemeConfig mCurrentTheme;
     private boolean mRecreating = false;
+
+    private int mBatterySaverWarningColor;
 
     // for disabling the status bar
     int mDisabled1 = 0;
@@ -585,9 +582,6 @@ public class PhoneStatusBar extends BaseStatusBar implements DemoMode,
                     Settings.System.STATUS_BAR_CUSTOM_HEADER_DEFAULT))) {
 					mStatusBarHeaderMachine.updateEnablement();
 	         		mStatusBarHeaderMachine.doUpdateStatusHeaderObservers(true);
-			} else if (uri.equals(Settings.System.getUriFor(
-                    Settings.System.USE_SLIM_RECENTS))) {
-                updateRecents();
             } else if (uri.equals(Settings.System.getUriFor(
                     Settings.System.USE_SLIM_RECENTS))) {
                 updateRecents();
