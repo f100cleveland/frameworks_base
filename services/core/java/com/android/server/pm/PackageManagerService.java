3178a1ba9ee5eaaadfa00b840e24640fcc3c1d0c
Corey Edwards <ensabahnur16@gmail.com>
2016-03-24 10:38:29 -0500

    Revert "Default to Resolver Theme"
    This reverts commit 922cc9c13df3679fbff59a696e526e930ec80234.
    
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- services/core/java/com/android/server/pm/PackageManagerService.java
+++ services/core/java/com/android/server/pm/PackageManagerService.java
@@ -8320,9 +8320,6 @@ public class PackageManagerService extends IPackageManager.Stub {
                     break;
                 }
             }
-            if (mResolveActivity.theme == 0) {
-                mResolveActivity.theme = R.style.Theme_DeviceDefault_Resolver;
-            }
         }
     }
 
